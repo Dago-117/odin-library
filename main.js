3ded69800dac9863e636d373f8f299e15d86dc26
David Garcia <go_mandi@outlook.com>
2025-02-03 20:50:40 -0600

    Add three basic files for web page and Book class

---
+++ main.js
@@ -0,0 +1,11 @@
+const mylibrary = [];
+
+class Book {
+    constructor(title, author, pages) {
+        this._title = title;
+        this._author = author;
+        this._pages = pages;
+        this._haveRead = false;
+    }
+}
+
