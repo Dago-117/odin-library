caefa65a935c8368ad51d973029019e7fc440ed3
David Garcia <go_mandi@outlook.com>
2025-02-06 22:47:17 -0600

    Add functionality to modal submit button; new row with data is added to table

--- dom.js
+++ dom.js
@@ -2,12 +2,13 @@ import myLibrary from "./library.js";
 import { Book } from "./library.js";
 
 const table = document.querySelector('tbody');
-const NUM_COLUMNS = Object.keys(Book).length;
 const LIB_SIZE = myLibrary.length;
 const newBtn = document.getElementById('add-book');
-
-
-
+const submitBtn = document.getElementById('submit-button');
+const cell = document.createElement('td');
+const authorInput = document.getElementById('author');
+const titleInput = document.getElementById('title');
+const pagesInput = document.getElementById('pages');
 
 for (let i = 0; i < LIB_SIZE; i++) {
     const book = myLibrary[i];
@@ -22,9 +23,6 @@ for (let i = 0; i < LIB_SIZE; i++) {
 }
 
 newBtn.addEventListener('click', e => {
-
-    const row = document.createElement('tr');
-    const cell = document.createElement('td');
     modal.style.display = 'flex';  // Use flex to center the modal content
     modal.focus(); // Bring focus into the modal for accessibility
 })
@@ -36,6 +34,27 @@ newBtn.addEventListener('mouseover', e => {
 modal.addEventListener('click', (e) => {
     if (e.target === modal) {
         modal.style.display = 'none';
-        openFormBtn.focus();
     }
 });
+
+submitBtn.addEventListener('click', e => {
+    e.preventDefault();
+    console.log("This button was clicked");
+    const row = document.createElement('tr');
+
+    table.appendChild(row);
+    const titleCell = document.createElement('td');
+    titleCell.innerText = titleInput.value;
+    row.appendChild(titleCell);
+
+    const authorCell = document.createElement('td');
+    authorCell.innerText = authorInput.value;
+    row.appendChild(authorCell);
+
+    const pagesCell = document.createElement('td');
+    pagesCell.innerText = pagesInput.value;
+    row.appendChild(pagesCell);
+    modal.style.display = 'none';
+
+
+})
